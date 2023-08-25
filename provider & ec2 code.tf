7cc5c4fee213a21ffff66b0f5e6913ec864ece66
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-25 11:21:52 -0400

    Update provider & ec2 code.tf
    changed local name to EC2

--- provider & ec2 code.tf
+++ provider & ec2 code.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "5lYq+fLGI3rldDaQAzAy3OmF48YQFPHkb5D6lpKn"
 }
 
-resource "aws_instance" "Devops-terraform" {
+resource "aws_instance" "EC2" {
 ami = "ami-0da59f1af71ea4ad2"
 instance_type = "t2.micro"
 }
