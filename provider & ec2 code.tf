2194705f08e5de5fba2be6374a7e84d0f6995e71
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-28 09:19:20 -0400

    Update provider & ec2 code.tf
    changed access key

--- provider & ec2 code.tf
+++ provider & ec2 code.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA3SCL7LLEQGXDAE7H"
-  secret_key = "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"
+  access_key = "AKIA3SCL7LLE4RCIJKOL"
+  secret_key = "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"
 }
 
 resource "aws_instance" "EC2" {
